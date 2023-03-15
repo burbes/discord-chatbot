fd0e34046a62404dd755155e09e3558a6ff5c711
Naelson Matheus Junior <burbes@gmail.com>
2023-03-15 16:55:38 -0300

    :bug: Fixed code

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import os
 import discord
 import openai
 
+# Set up OpenAI API
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
 
-class ChatBot (discord.Client):
+intents = discord.Intents.default()
+intents.messages = True
+
+class ChatBot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     async def on_ready(self):
         print(f'{self.user} is connected to Discord!')
 
@@ -14,21 +21,20 @@ class ChatBot (discord.Client):
         if message.author == self.user:
             return
 
-        input_content = [message.content]
+        input_content = message.content
 
         if message.attachments:
             for attachment in message.attachments:
                 image_bytes = await attachment.read()
-                input_content.append({"image": image_bytes})
+                input_content += f" {attachment.filename}"
 
         response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[{"role": " ee ", “content”: input_content}]
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": input_content}]
         )
 
         assistant_response = response['choices'][0]['message']['content']
         await message.channel.send(assistant_response)
 
-client = ChatBot()
+client = ChatBot(intents=intents)
 client.run(DISCORD_TOKEN)
-
